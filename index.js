a8adcf009d3824c1c6c663f981071424152a5cd6
GASgod1111 <reagan.peter@student.moringaschool.com>
2023-06-17 10:34:47 +0300

    index.html

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// Function expression: divide
+const divide = () => {
+    return 2000 / 100;
+  };
+  
+  // Arrow function: square
+  const square = (num) => {
+    return num * num;
+  };
+  
+  // Arrow function: add
+  const add = (a, b) => {
+    return a + b;
+  };
+  
